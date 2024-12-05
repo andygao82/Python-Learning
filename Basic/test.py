2252f834460f20ad1ac37e059196040626fb82a1
andygao82 <freedomandy82@gmail.com>
2024-12-05 16:17:57 +1100

    Update test.py

--- Basic/test.py
+++ Basic/test.py
@@ -4,5 +4,14 @@
   print('hello world');
 """
 abc =  1123;
+year = 2024
+month = 11
+day = 30  
+
+day = "Saturday"
 
 print(abc);
+
+print(abc, 'numeric');
+
+print("Today is %d-%d-%d, %s" % (day, month, year, day) )
